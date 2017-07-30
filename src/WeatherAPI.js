7b60c3e932decdadf1c6dc520c87fad95c288970
Krishna <krishna444@gmail.com>
2017-07-30 23:28:24 +0200

    concealed changes

--- src/WeatherAPI.js
+++ src/WeatherAPI.js
@@ -1,6 +1,6 @@
 import jQuery from 'jquery';
 var cityList = require('./city.list.json');
-const APPID = "49388ab64d987b4141722396747fad67";
+const APPID = "****";
 const URL = "http://api.openweathermap.org/data/2.5/weather";
 
  export default class WeatherAPI {
