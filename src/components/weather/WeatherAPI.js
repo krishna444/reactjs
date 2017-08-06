5c0c2c69ed6b2c43e3fce50b7fdb31d1ca378818
krishna-windows <krishna444@gmail.com>
2017-08-06 15:42:12 +0200

    updates dynamic country and updates

--- src/components/weather/WeatherAPI.js
+++ src/components/weather/WeatherAPI.js
@@ -1,27 +1,48 @@
 import jQuery from 'jquery';
 var cityList = require('../../static/city.list.json');
-var config=require('../../static/config.json');
+var config = require('../../static/config.json');
 
 const APPID = config.appId.weather;
 const URL = "http://api.openweathermap.org/data/2.5/weather";
 
- export default class WeatherAPI {
-    constructor() {        
- 
-    }
+export default class WeatherAPI {
+    constructor() {
+
+    };
+
+    getCountryList() {
+        let countries = [];
+        cityList.map(city => {
+           // console.log(city)
+            if (jQuery.inArray(city.country, countries) == -1) {
+             //   console.log("inside");
+                countries.push(city.country);
+            }
+        })
+        return countries;
+    };
 
     getCityInfo(city, country) {
         let filteredList = cityList.filter(cityInfo => cityInfo.name == city && cityInfo.country == country);
-        if(filteredList.length==0){
+        if (filteredList.length == 0) {
             console.error("CITY NOT FOUND");
             return;
         }
         return filteredList[0];
-    }
+    };
 
-    getWeather(id,callback) {
-        let fetchURL = URL + "?units=metric&id=" + id + "&appid=" + APPID;  
+    getCities(country) {
+        console.log("Getting cities for " + country);
+        let filteredList = cityList.filter(cityInfo => cityInfo.country == country)
+        if (filteredList.length == 0) {
+            console.error("cities not found");
+        }
+        return filteredList;
+    };
+
+    getWeather(id, callback) {
+        let fetchURL = URL + "?units=metric&id=" + id + "&appid=" + APPID;
         console.log(fetchURL);
-        jQuery.getJSON(fetchURL,callback)
+        jQuery.getJSON(fetchURL, callback)
     }
 }
