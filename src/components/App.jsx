ce9e1abfc261c24b124520e61817baf18ee9242b
krishna@dev <krishna444@gmail.com>
2018-08-12 01:27:21 +0200

    updates

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import '../static/App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1><marquee>Another app</marquee></h1>
+            </div>
+        );
+    }
+}
+
+module.exports = {App};
