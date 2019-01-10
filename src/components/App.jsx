0cb8f9650505a452054b210d61194fde4ab6d076
krishna@dev <krishna444@gmail.com>
2019-01-10 23:08:18 +0100

    modified

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ class App extends Component {
     render() {
         return (
             <div className="jumbotron">
-                <h1><marquee>Another app</marquee></h1>
+                <h1><marquee>Another app1</marquee></h1>
             </div>
         );
     }
